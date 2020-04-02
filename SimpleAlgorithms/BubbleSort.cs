71084fe4367c38aca5b30033e5ef2687b06d0a16
Maxim Isaev <iwork30rus@gmail.com>
2020-04-02 21:04:40 +0400

    Add new algorithm sorted
    Algorithm BubbleSort

---
+++ SimpleAlgorithms/BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAlgorithms
+{
+    class BubbleSort
+    {
+        public void Search(int[] array)
+        {
+            for(int i = array.Length - 1; i > 0; i--)
+                for(int j = 1; j <= i; j++)
+                    if(array[j-1] > array[j])
+                    {
+                        var temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                    }
+        }
+    }
+}
